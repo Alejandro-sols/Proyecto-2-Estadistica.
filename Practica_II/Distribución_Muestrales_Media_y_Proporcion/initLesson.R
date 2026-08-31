82dc72a1493a62191b582db5766b4c446ec2a5e4
Gael2207 <156036812+Gael2207@users.noreply.github.com>
2026-08-31 16:39:32 -0600

    Add files via upload
    Esta carpeta tiene una de las lecciones que hice, combina la 7 y la 8

--- Practica_II/Distribución_Muestrales_Media_y_Proporcion/initLesson.R
+++ Practica_II/Distribución_Muestrales_Media_y_Proporcion/initLesson.R
@@ -1,4 +1,14 @@
-# Code placed in this file fill be executed every time the
-      # lesson is started. Any variables created here will show up in
-      # the user's working directory and thus be accessible to them
-      # throughout the lesson.
+set.seed(2026)
+
+# Poblacion 1: Tiempos de ejecucion de procesos en un servidor (milisegundos)
+poblacionTiempos <- round(rnorm(1000, mean = 250, sd = 30), 2)
+
+# Poblacion 2: Estado de peticiones HTTP en una API (1 = Exitosa, 0 = Fallida)
+# Proporci0n real de peticiones exitosas p = 0.88
+poblacionPeticiones <- sample(c(1, 0), size = 1000, replace = TRUE, prob = c(0.88, 0.12))
+
+# Peticiones del servidor: 85% exitosas (1), 15% con error (0)
+peticionesServidor <- sample(c(1, 0), size = 10000, replace = TRUE, prob = c(0.85, 0.15))
+
+# Datos auxiliares
+N <- 1000
