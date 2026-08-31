e13966b67ed29aad6fb100e34a8469bd6138d5ba
Gael2207 <156036812+Gael2207@users.noreply.github.com>
2026-08-31 16:40:21 -0600

    Add files via upload
    Esta es la ultima leccion, la 9

---
+++ Practica_II/Distribucion_muestral_de_la_varianza/initLesson.R
@@ -0,0 +1,7 @@
+# Code placed in this file fill be executed every time the
+      # lesson is started. Any variables created here will show up in
+      # the user's working directory and thus be accessible to them
+      # throughout the lesson.
+
+set.seed(123)
+poblacionTiempos <- rnorm(5000, mean = 150, sd = 14.14) 
