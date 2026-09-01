8e9232186e97d071ceac78585ac3e6706d233c67
Alejandro-sols <sergio.sol.ast@gmail.com>
2026-09-01 11:24:28 -0600

    Agrega descripcion de initLesson

--- Practica_II/Muestreo Estratificado/initLesson.R
+++ Practica_II/Muestreo Estratificado/initLesson.R
@@ -1,4 +1,14 @@
+
+# Se utiliza semilla, ya que en la base de datos simulada 'bd_usuarios' se le 
+# asigna valor aleatorio y de esta forma aseguramos que los datos mostrados al estudiante
+# sean exactos con los que hicimos el curso
 set.seed(5585)
+
+# Se crea un objeto que simula una base de datos que guarda a 1000 usuarios,
+# cada usuario contiene de atributo en que dispositivo usa la suscripcion 
+# y que tipo de suscripcion tiene
+
+# Se utiliza para que el estudiante extraiga un estrato con los usuarios pro y despues con estos crea una muestra aleatoria.
 
 bd_usuarios <- data.frame(
   id = 1:1000,
