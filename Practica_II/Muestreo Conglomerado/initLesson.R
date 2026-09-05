c89de132a741ea1191fe2896635dcff6d44bd581
Alejandro-sols <sergio.sol.ast@gmail.com>
2026-09-05 16:28:53 -0600

    Agrega descripcion a init

--- Practica_II/Muestreo Conglomerado/initLesson.R
+++ Practica_II/Muestreo Conglomerado/initLesson.R
@@ -1,3 +1,9 @@
+#Se simula una base de datos que contiene 50 servidores
+#Que se dividen en 5 conjuntos de 10 cada uno con sus respectivos tiempos de carga cada servidor 
+
+#El estudiante utilizara esto para sacar una muestra conglomerada de 2 de los 5 conjuntos
+
+#En este caso no se utiliza una semilla ya que no necesitamos datos previamente calculados para el curso
 
 
 bd_transacciones <- data.frame(
